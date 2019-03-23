9d4665eea4a691cb3b6151e8c2d0c4e9f8782768
BendyMAIN <48861183+BendyMAIN@users.noreply.github.com>
2019-03-23 20:45:52 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,16 +1,48 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
-import asyncio 
-client = commands.Bot(command_prefix='/') 
-print ("Loading....") 
+import asyncio
+
+client = commands.Bot(command_prefix='/')
+
+print ("Loading....")
+
 @client.event
-async def on_ready(): 	print ("The bot is online!") 	await client.change_presence(game=discord.Game(name='BENDY BOT | v1.0', type=3))  
+async def on_ready():
+	print ("The bot is online!")
+	await client.change_presence(game=discord.Game(name='BENDY BOT | v1.0', type=3))
+	
 @client.command()
-async def ping():     await client.say("Pong!")  
+async def ping():
+    await client.say("Pong!")
+ 
 @client.command(pass_context=True)
-async def info(ctx, user: discord.Member):     await client.say("Felhasználói információk")     await client.say("Név: {}".format(user.name))     await client.say("Id: {}".format(user.id))     await client.say("Legmagasabb rang: {}".format(user.top_role))     
-async def piros():     embed = discord.Embed(         title = 'Cím',         description = 'Leírás',         colour = discord.Colour.red()     )     embed.add_field(name='Név', value='Érték', inline=False)     embed.add_field(name='Név', value='Érték', inline=False)     embed.set_footer(text='TutorialBot | v1.0')     await client.say(embed=embed)    
+async def info(ctx, user: discord.Member):
+    await client.say("Felhasználói információk")
+    await client.say("Név: {}".format(user.name))
+    await client.say("Id: {}".format(user.id))
+    await client.say("Legmagasabb rang: {}".format(user.top_role))
+    
+async def piros():
+    embed = discord.Embed(
+        title = 'Cím',
+        description = 'Leírás',
+        colour = discord.Colour.red()
+    )
+    embed.add_field(name='Név', value='Érték', inline=False)
+    embed.add_field(name='Név', value='Érték', inline=False)
+    embed.set_footer(text='TutorialBot | v1.0')
+    await client.say(embed=embed)
+   
 @client.command(pass_context=True)
-async def infó():     embed = discord.Embed(         title = 'Információk',         description = 'Parancsaim: /info',         colour = discord.Colour.green()     )     embed.add_field(name='By', value='BENDY', inline=False)     embed.set_footer(text='BENDY BOT|v1.0')     await client.say(embed=embed) 
+async def infó():
+    embed = discord.Embed(
+        title = 'Információk',
+        description = 'Parancsaim: /info',
+        colour = discord.Colour.green()
+    )
+    embed.add_field(name='By', value='BENDY', inline=False)
+    embed.set_footer(text='BENDY BOT|v1.0')
+    await client.say(embed=embed)
+
 client.run("NTU2NTQ4NzM3MDM2NDUxODQ2.D27dWA.lp6BOu5OViE8_RXECgoj9tmi5xU")
