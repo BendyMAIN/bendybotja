68ec768beb7a9ccf3ba4ed91037a5aea3df2cb90
BendyMAIN <48861183+BendyMAIN@users.noreply.github.com>
2019-04-01 17:22:25 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -44,5 +44,20 @@ async def segítség():
     embed.add_field(name='By', value='BENDY', inline=False)
     embed.set_footer(text='BENDY BOT|v1.0')
     await client.say(embed=embed)
+
+@bot.command()
+async def info(ctx):
+embed = discord.Embed(title="nice bot", description="Nicest bot there is ever.", color=0xeee657)
+
+# give info about you here
+embed.add_field(name="Author", value="<YOUR-USERNAME>")
+
+# Shows the number of servers the bot is member of.
+embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
+
+# give users a link to invite thsi bot to their server
+embed.add_field(name="Invite", value="[Invite link](<insert your OAuth invitation link here>)")
+
+await ctx.send(embed=embed)
 	
 client.run("NTU2NTQ4NzM3MDM2NDUxODQ2.D27dWA.lp6BOu5OViE8_RXECgoj9tmi5xU") 
